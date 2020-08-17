50fabb9092d7becb86b7b1dd3b0107e633ccf149
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-08-17 14:55:44 +0000

    carousel updated to react-bootstrap

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import ReactBootstrap from "react-bootstrap";
+import { render } from "react-dom";
+import Carousel from "react-bootstrap/Carousel";
 import { Link } from "react-router-dom";
 import companyLogo from "../../img/supergig-logo.png";
 
@@ -8,35 +11,45 @@ import { Home } from "../views/home";
 // Importing styles*/
 import "../../styles/components.scss";
 
-export const Carousel = () => {
-    return (
-        <div className="carousel-container">
-            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img className="d-block w-100" src="..." alt="First slide"/>
-                    </div>
-                    <div className="carousel-item">
-                        <img className="d-block w-100" src="..." alt="Second slide"/>
-                    </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="..." alt="Third slide"/>
-                    </div>
-                        </div>
-                        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                    </div>
-    );
-};
+export function ControlledCarousel() {
+	const [index, setIndex] = useState(0);
+
+	const handleSelect = (selectedIndex, e) => {
+		setIndex(selectedIndex);
+	};
+	return (
+		<Carousel activeIndex={index} onSelect={handleSelect}>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src="https://images.unsplash.com/photo-1502945015378-0e284ca1a5be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+					alt="First slide"
+				/>
+				<Carousel.Caption>
+					<h3>First slide label</h3>
+					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className="d-block w-100"
+					src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+					alt="Second slide"
+				/>
+
+				<Carousel.Caption>
+					<h3>Second slide label</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img className="d-block w-100" src="holder.js/800x400?text=Third slide&bg=20232a" alt="Third slide" />
+
+				<Carousel.Caption>
+					<h3>Third slide label</h3>
+					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+				</Carousel.Caption>
+			</Carousel.Item>
+		</Carousel>
+	);
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
+import { ControlledCarousel } from "./../component/carousel";
 
 export const Home = () => {
 	return (
 		<div className="home-container d-flex justify-content-center">
-			<p>WELCOME TO THE SUPERGIG WEBSITE.</p>
+			<ControlledCarousel />
 		</div>
 	);
 };
