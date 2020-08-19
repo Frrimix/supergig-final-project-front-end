74d039193b8cdceb9cba01c491e501c604363b9a
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-08-19 15:33:31 +0000

    styling updates for login form

--- src/js/component/logInForm.js
+++ src/js/component/logInForm.js
@@ -12,11 +12,13 @@ import "../../styles/components.scss";
 export const LogInForm = () => {
 	return (
 		<div className="logInForm-container">
+			{/* Image container - L hand side */}
 			<div className="logIn-second-container d-flex justify-content-around">
 				<img
 					className="logInForm-img"
 					src="https://image.freepik.com/free-vector/vector-illustration-woman-sitting-front-computer-working-project-searching-chating_101484-63.jpg"
 				/>
+				{/* Log-in Form - R hand side */}
 				<Form>
 					<h1 className="d-flex justify-content-center">Log-In</h1>
 					<br />
@@ -33,17 +35,17 @@ export const LogInForm = () => {
 					<Form.Group controlId="formBasicCheckbox">
 						<Form.Check type="checkbox" label="Remember Me" />
 					</Form.Group>
-					<Button variant="primary" type="submit">
+					<Button className="submit-button" variant="primary" type="submit">
 						Submit
 					</Button>
 				</Form>
 			</div>
-			<div className="logInForm-create-account">
-				<Link to={"/create-account"} href="#">
-					Create an account
-				</Link>
-				<p></p>
-			</div>
+
+			{/* Create Account Link */}
+			<Link to={"/create-account"} href="#">
+				<a className="logInForm-create-account">Create an account</a>
+			</Link>
+			<p></p>
 		</div>
 	);
 };
