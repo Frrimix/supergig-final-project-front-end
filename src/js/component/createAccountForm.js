3d0bf5abdbf8130bb20eb2ebe65ad46469eacb67
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-08-19 16:42:12 +0000

    update to create account form

--- src/js/component/createAccountForm.js
+++ src/js/component/createAccountForm.js
@@ -16,7 +16,7 @@ export const CreateAccountForm = () => {
 			<div className="createAccount-second-container d-flex justify-content-around">
 				<img
 					className="createAccountForm-img"
-					src="https://image.freepik.com/free-vector/vector-illustration-woman-sitting-front-computer-working-project-searching-chating_101484-63.jpg"
+					src="https://img.freepik.com/free-vector/vector-illustration-woman-sitting-front-computer-working-project_101484-102.jpg?size=626&ext=jpg"
 				/>
 				{/* Log-in Form - R hand side */}
 				<Form>
@@ -60,7 +60,56 @@ export const CreateAccountForm = () => {
 							<Form.Label>State</Form.Label>
 							<Form.Control as="select" defaultValue="Choose...">
 								<option>Choose...</option>
-								<option>...</option>
+								<option value="1">Alabama</option>
+								<option value="2">Alaska</option>
+								<option value="3">Arizona</option>
+								<option value="4">Arkansas</option>
+								<option value="5">California</option>
+								<option value="6">Colorado</option>
+								<option value="7">Connecticut</option>
+								<option value="8">Delaware</option>
+								<option value="9">Florida</option>
+								<option value="10">Georgia</option>
+								<option value="11">Hawaii</option>
+								<option value="12">Idaho</option>
+								<option value="13">Illinois</option>
+								<option value="14">Indiana</option>
+								<option value="15">Iowa</option>
+								<option value="16">Kansas</option>
+								<option value="17">Kentucky</option>
+								<option value="18">Louisiana</option>
+								<option value="19">Maine</option>
+								<option value="20">Maryland</option>
+								<option value="21">Massachusetts</option>
+								<option value="22">Michigan</option>
+								<option value="23">Minnesota</option>
+								<option value="24">Mississippi</option>
+								<option value="25">Missouri</option>
+								<option value="26">Montana</option>
+								<option value="27">Nebraska</option>
+								<option value="28">Nevada</option>
+								<option value="29">New Hampshire</option>
+								<option value="30">New Jersey</option>
+								<option value="31">New Mexico</option>
+								<option value="32">New York</option>
+								<option value="33">North Carolina</option>
+								<option value="34">North Dakota</option>
+								<option value="35">Ohio</option>
+								<option value="36">Oklahoma</option>
+								<option value="37">Oregon</option>
+								<option value="38">Pennsylvania</option>
+								<option value="39">Rhode Island[</option>
+								<option value="40">South Carolina</option>
+								<option value="41">South Dakota</option>
+								<option value="42">Tennessee</option>
+								<option value="43">Texas</option>
+								<option value="44">Utah</option>
+								<option value="45">Vermont</option>
+								<option value="46">Virginia</option>
+								<option value="47">Washington</option>
+								<option value="48">West Virginia</option>
+								<option value="49">Wisconsin</option>
+								<option value="50">Wyoming</option>
 							</Form.Control>
 						</Form.Group>
 
@@ -94,19 +143,16 @@ export const CreateAccountForm = () => {
 						</Form.Group>
 					</Form.Row>
 
-					<br />
-
-					<Button variant="primary" type="submit">
+					<Button className="submit-button" variant="primary" type="submit">
 						Submit
 					</Button>
 				</Form>
 			</div>
 
 			{/* Create Account Link */}
-			<Link to={"/create-account"} href="#">
-				<a className="logInForm-create-account">Create an account</a>
+			<Link to={"/log-in"} href="#">
+				<a className="createAcountForm-Login">Log-In</a>
 			</Link>
-			<p></p>
 		</div>
 	);
 };
