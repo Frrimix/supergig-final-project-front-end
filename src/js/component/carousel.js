27049759fdf1ee159a1627770a2b77dad9a3461f
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-08-31 16:03:11 +0000

    carousel captions added

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -33,8 +33,10 @@ export function ControlledCarousel() {
 				<Carousel.Item>
 					<img className="carousel-img" src={firstPhoto} alt="First slide" />
 					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+						<div className="caption-background">
+							<h3>Welcome to SuperGig</h3>
+							<p>We connect members of your community who need help with a job or are looking to work.</p>
+						</div>
 					</Carousel.Caption>
 				</Carousel.Item>
 
@@ -42,8 +44,10 @@ export function ControlledCarousel() {
 				<Carousel.Item>
 					<img className="carousel-img" src={secondPhoto} alt="Second slide" />
 					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<div className="caption-background">
+							<h3>Hassle-Free</h3>
+							<p>We make the process of finding the right local person for your job easy.</p>
+						</div>
 					</Carousel.Caption>
 				</Carousel.Item>
 
@@ -51,8 +55,13 @@ export function ControlledCarousel() {
 				<Carousel.Item>
 					<img className="carousel-img" src={thirdPhoto} alt="Third slide" />
 					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						<div className="caption-background">
+							<h3>Growing Community</h3>
+							<p>
+								With our growing community you will be sure to find the right person for your job or the
+								right job for your expertise.
+							</p>
+						</div>
 					</Carousel.Caption>
 				</Carousel.Item>
 			</Carousel>
