714a9adda12247da39899ccd7c8b130bee649ec4
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-09-06 19:25:00 +0000

    zip code search bar added to job view page

--- src/js/views/jobView.js
+++ src/js/views/jobView.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import ReactBootstrap, { Dropdown, DropdownButton } from "react-bootstrap";
+import ReactBootstrap, { Dropdown, DropdownButton, Form, FormControl, Button } from "react-bootstrap";
 import "../../styles/jobView.scss";
 import { JobViewC } from "./../component/jobViewC";
 
 export const JobView = () => {
 	return (
 		<div className="jobView-wrapper">
-			<DropdownButton className="jobView-button" title="Sort by">
-				<Dropdown.Item href="#/action-1">Closest Location</Dropdown.Item>
-				<Dropdown.Item href="#/action-2">Highest Paying</Dropdown.Item>
-				<Dropdown.Item href="#/action-3">Most Recent</Dropdown.Item>
-			</DropdownButton>
-			<div className="jobView-container d-flex justify-content-center">
+			<div className="jobView-buttons d-flex justify-content-center">
+				<Form.Control className="jobView-zipcode-search" type="text" placeholder="Enter your zipcode" />
+				<Button className="jobView-zipcode-search-button" href="#">
+					Search
+				</Button>
+				<DropdownButton id="dropdown-button" title="Sort by">
+					<Dropdown.Item href="#/action-1">Closest Location</Dropdown.Item>
+					<Dropdown.Item href="#/action-2">Highest Paying</Dropdown.Item>
+					<Dropdown.Item href="#/action-3">Most Recent</Dropdown.Item>
+				</DropdownButton>
+			</div>
+			<div className="jobView-container d-flex justify-content-center ">
 				<JobViewC />
 			</div>
 		</div>
