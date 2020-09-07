8802ce87daf235cc00de1e7ee9260694a501101f
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-09-07 17:00:36 +0000

    pagination component added and formatted

--- src/js/component/pagination.js
+++ src/js/component/pagination.js
@@ -7,20 +7,43 @@ import companyLogo from "../../img/supergig-logo.png";
 // Importing styles*/
 import "../../styles/components.scss";
 
-let active = 1;
-let items = [];
-for (let number = 1; number <= 5; number++) {
-	items.push(
-		<Pagination.Item key={number} active={number === active}>
-			{number}
-		</Pagination.Item>
-	);
-}
-
 export const PaginationC = () => {
 	return (
 		<div className="pagination-container">
-			<Pagination>{items}</Pagination>
+			<nav aria-label="Page navigation example">
+				<ul className="pagination pg-green">
+					<li className="page-item ">
+						<a className="page-link" tabIndex="-1" href="#">
+							Previous
+						</a>
+					</li>
+					<li className="page-item active">
+						<a className="page-link" href="#">
+							1 <span className="sr-only">(current)</span>
+						</a>
+					</li>
+					<li className="page-item">
+						<a className="page-link" href="#">
+							2
+						</a>
+					</li>
+					<li className="page-item">
+						<a className="page-link" href="#">
+							3
+						</a>
+					</li>
+					<li className="page-item">
+						<a className="page-link" href="#">
+							4
+						</a>
+					</li>
+					<li className="page-item ">
+						<a className="page-link" href="#">
+							Next
+						</a>
+					</li>
+				</ul>
+			</nav>
 		</div>
 	);
 };
