8e29f382dbc485e47d925492b7aaa696cccd1e07
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-09-12 14:17:39 +0000

    endpoints created on flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,59 @@
+const url = "https://3000-f14d7ffd-dd58-412b-b623-a3ac36359ebf.ws-us02.gitpod.io/";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			token: null,
+			currentUser: null
 		},
+
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			// Log-out function
+			logout: () => {
+				setStore({ token: null });
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+			// Logi-in function
+			login: (test_email, test_password, history) => {
+				fetch(url + "login/", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						email: test_email,
+						password: test_password
+					})
+				})
+					.then(response => {
+						if (response.status === 200) return response.json();
+						alert("Username or password does not match any");
+						throw Error('"Username or password does not match any"');
+					})
+					.then(data => {
+						setStore({ token: data.jwt, currentUser: data.user });
+						history.push("user-profile/");
+					});
+			},
+
+			// Add a user function
+			addUser: (email, first_name, last_name, password, address, zipcode) => {
+				fetch(url + "user/", {
+					method: "POST",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						email: email,
+						first_name: first_name,
+						last_nam: last_nam,
+						password: password,
+						address: address,
+						zipcode: zipcode,
+						type_of_user: "user"
+					})
+				}).then(() => {
+					getActions().getUser();
 				});
-
-				//reset the global store
-				setStore({ demo: demo });
 			}
+			//reset the global store
+			// setStore({ demo: demo })
 		}
 	};
 };
